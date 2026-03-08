db2767da88b78b0852ad90daab37409eac9c2681
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-03-07 22:20:47 -0600

    added options for max depth recursion

--- src/Pyleft/Driver/Run.hs
+++ src/Pyleft/Driver/Run.hs
@@ -1,25 +1,33 @@
 module Pyleft.Driver.Run (main) where
 
 import Data.Aeson (Value, eitherDecode)
+import Pyleft.CLI.Options (Options (..), parseOptions)
+import Pyleft.Driver.Discover (discoverPythonFiles)
 import Pyleft.Frontend.Parse (parseFileToAstJson)
 import Pyleft.Lint.Registry (runAllRules)
 import Pyleft.Report.Format (formatDiagnosticPure)
-import System.Environment (getArgs)
 import System.IO (hIsTerminalDevice, stdout)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [path] -> runOne path
-    _ -> putStrLn "usage: pyleft <file.py>"
+  opts <- parseOptions
+  runPaths (optMaxDepth opts) (optPaths opts)
 
-runOne :: FilePath -> IO ()
-runOne path = do
+runPaths :: Int -> [FilePath] -> IO ()
+runPaths maxDepth paths = do
+  files <- discoverPythonFiles maxDepth paths
+  useColor <- hIsTerminalDevice stdout
+
+  case files of
+    [] -> putStrLn "No Python files found."
+    _ -> mapM_ (runOne useColor) files
+
+runOne :: Bool -> FilePath -> IO ()
+runOne useColor path = do
   jsonBytes <- parseFileToAstJson path
   case eitherDecode jsonBytes :: Either String Value of
-    Left e -> putStrLn ("Failed to decode AST JSON: " <> e)
+    Left e ->
+      putStrLn (path <> ": Failed to decode AST JSON: " <> e)
     Right astVal -> do
       let diags = runAllRules path astVal
-      useColor <- hIsTerminalDevice stdout
       mapM_ (putStrLn . formatDiagnosticPure useColor) diags
