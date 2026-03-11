6e262b252fe73b53f0db86a394038d40ffbd24b9
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-03-11 14:45:19 -0500

    added threading support

--- src/Pyleft/Driver/Run.hs
+++ src/Pyleft/Driver/Run.hs
@@ -1,10 +1,13 @@
 module Pyleft.Driver.Run (main) where
 
+import Control.Concurrent.Async (mapConcurrently)
+import Control.Concurrent.QSem
+import Control.Exception (bracket_)
 import Data.Aeson (Value, eitherDecode)
 import Pyleft.CLI.Options (Options (..), parseOptions)
 import Pyleft.Driver.Discover (discoverPythonFiles)
 import Pyleft.Frontend.Parse (parseFileToAstJson)
-import Pyleft.Lint.Config (LintConfig, applyConfig, cfgTheme, loadConfig)
+import Pyleft.Lint.Config (LintConfig (..), applyConfig, loadConfig)
 import Pyleft.Lint.Registry (runAllRules)
 import Pyleft.Report.Format (formatDiagnosticPure)
 import System.IO (hIsTerminalDevice, stdout)
@@ -22,14 +25,28 @@ runPaths cfg maxDepth paths = do
 
   case files of
     [] -> putStrLn "No Python files found."
-    _ -> mapM_ (runOne cfg useColor) files
+    _ -> do
+      outputs <- mapConcurrentlyBoundedIO 8 (runOne cfg useColor) files
+      mapM_ putStr outputs
 
-runOne :: LintConfig -> Bool -> FilePath -> IO ()
+runOne :: LintConfig -> Bool -> FilePath -> IO String
 runOne cfg useColor path = do
   jsonBytes <- parseFileToAstJson path
   case eitherDecode jsonBytes :: Either String Value of
     Left e ->
-      putStrLn (path <> ": Failed to decode AST JSON: " <> e)
+      pure (path <> ": Failed to decode AST JSON: " <> e <> "\n")
     Right astVal -> do
       let diags = applyConfig cfg (runAllRules path astVal)
-      mapM_ (putStrLn . formatDiagnosticPure useColor (cfgTheme cfg)) diags
+      pure $
+        unlines (map (formatDiagnosticPure useColor (cfgTheme cfg)) diags)
+
+mapConcurrentlyBoundedIO :: Int -> (a -> IO b) -> [a] -> IO [b]
+mapConcurrentlyBoundedIO maxThreads action xs = do
+  sem <- newQSem maxThreads
+  mapConcurrently (withSem sem . action) xs
+  where
+    withSem :: QSem -> IO b -> IO b
+    withSem sem =
+      bracket_
+        (waitQSem sem)
+        (signalQSem sem)
